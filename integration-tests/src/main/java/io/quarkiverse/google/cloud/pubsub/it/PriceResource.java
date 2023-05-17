40518265856247961e36322d77817a69e031a601
zZHorizonZz <danfiala23@gmail.com>
2023-05-17 10:39:02 +0200

    enhancement(test): Added outgoing channel

--- integration-tests/src/main/java/io/quarkiverse/google/cloud/pubsub/it/PriceResource.java
+++ integration-tests/src/main/java/io/quarkiverse/google/cloud/pubsub/it/PriceResource.java
@@ -1,28 +1,21 @@
 package io.quarkiverse.google.cloud.pubsub.it;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.spi.Connector;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
-import io.quarkiverse.google.cloud.pubsub.PubSubConnector;
+import io.smallrye.reactive.messaging.annotations.Broadcast;
 
 @ApplicationScoped
 public class PriceResource {
 
-    @Inject
-    @Connector("smallrye-gcp-pubsub")
-    PubSubConnector connector;
-
-    @PostConstruct
-    public void init() {
-        System.out.println("PriceResource init");
-    }
+    private static final double CONVERSION_RATE = 0.88;
 
     @Incoming("prices")
-    public void incoming(String data) {
-        System.out.println("Received: " + data);
+    @Outgoing("processed-prices")
+    @Broadcast
+    public double process(int priceInUsd) {
+        return priceInUsd * CONVERSION_RATE;
     }
 }
